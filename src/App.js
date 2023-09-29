dcf0c73d79e3181b0bc373bbff4089982b683852
Pavel Loubek <pav.loubek@gmail.com>
2023-09-29 02:15:15 +0200

    Added main.js and home.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/header/navbar.js';
+import Main from './components/main/main.js';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Main/>
     </div>
   );
 }
---
+++ src/components/main/main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Home from './Home/home.js';
+
+const Main = () => {
+  return (
+    <Home/>
+  );
+}
+
+export default Main;
