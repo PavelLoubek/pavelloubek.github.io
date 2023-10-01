ba69b1209c87af5e52f23d3124b7a8ae6b07d0e2
Pavel Loubek <pav.loubek@gmail.com>
2023-10-01 20:42:12 +0200

    Edited more of home.css and .js, added portrait
    Close to final look

--- src/components/main/Home/home.js
+++ src/components/main/Home/home.js
@@ -2,16 +2,20 @@ import React from 'react';
 import './home.css';
 import linkedInLogo from '../../../images/linkedInHomeLogoLight.png';
 import { Link } from 'react-scroll';
+import portraitPhoto from '../../../images/homePortrait.png'
 
 const Home = () => {
   return (
     <section id="homeSection">
-      <p className="hello">Hello!</p>
-      <p className="myNameIs">My name is <span className="name">Pavel Loubek</span></p>
-      <p className="doStuff">and I do various stuff from IT!</p>
-      <a href="https://www.linkedin.com/in/pavelloubek/"><img src={linkedInLogo} alt="LinkedIn" className="linkedInLogo"/></a>
+      <div className="homeMainText">
+        <p className="hello">Hello!</p>
+        <p className="myNameIs">My name is <span className="homeName">Pavel Loubek</span></p>
+        <p className="doStuff">and I do various stuff from IT!</p>
+        <a className="linkedInBtn" href="https://www.linkedin.com/in/pavelloubek/"><img src={linkedInLogo} alt="LinkedIn" className="linkedInHomeLogo"/></a>
+        <Link className="sectionScrollTo">More info below!</Link>
+      </div>
       <br/>
-      {/*<Link className='navbarScrollToBtn'>More info below!</Link>*/}
+      <img src={portraitPhoto} alt="PortraitPhoto" className="homePortrait"/>
     </section>
   );
 }
